126b95a42ed4b67b634939ed8800220b7fd06bb2
orondon <oscar.rondon.c@gmail.com>
2023-02-05 15:36:28 -0300

    Module 01 Complete

---
+++ dist/Module 01 Exercises/module01.js
@@ -0,0 +1,8 @@
+"use strict";
+function holaMundo() {
+    return "Hola Mundo!!!";
+}
+function addNumbers(x, y) {
+    return x + y;
+}
+console.log(addNumbers(3, 6));
---
+++ src/Module 01 Exercises/module01.ts
@@ -0,0 +1,9 @@
+function holaMundo(): string {
+    return "Hola Mundo!!!"
+}
+
+function addNumbers(x: number, y: number) {
+    return x + y
+}
+
+console.log(addNumbers(3, 6))
